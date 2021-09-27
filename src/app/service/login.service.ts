c877f5360056e047dde81b37788275bb1112a0d9
Your NameJatender <kumarjatender0@gmail.com>
2021-09-27 18:52:47 +0500

    Complete Login Form with Angular as a Front end and Spring Boot as a backend

--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class LoginService {
-  url = "http://localhost:8080";
+  url = "http://localhost:8081";
 
   constructor(private http: HttpClient) { }
 
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class UserService {
-baseUrl = 'http://localhost:8080'
+baseUrl = 'http://localhost:8081'
   constructor(private http:HttpClient) { }
   getUsers()
   {
